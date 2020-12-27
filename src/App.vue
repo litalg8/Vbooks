9abe9362015ff3d9dd1040cbbaacdadb7cdd5ae6
litalg8 <68659368+litalg8@users.noreply.github.com>
2020-12-27 21:55:55 +0200

    vue-books

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <app-header />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/cmps/AppHeader';
+import AppFooter from '@/cmps/AppFooter';
+import UserMsg from '@/cmps/UserMsg';
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+    UserMsg, 
+  },
+};
+</script>
